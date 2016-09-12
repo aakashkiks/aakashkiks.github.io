ff68014ad0beecf1bbfdcb9e34d91c6cda4db164
Aakash Kikani <Aakash Kikani>
2016-09-12 20:08:48 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,131 @@
+function popShow() {
+	var x = document.getElementById("pop");
+	x.style.height="200px";
+	var y = document.getElementById('im1').src = "tbbt2.png";
+	var z = document.getElementById("body");
+	z.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)),url('tbbt1.jpg')";
+	z.style.backgroundSize = "cover";
+	var logo1= document.getElementById("logo1");
+	logo1.style.display = "inline";
+	var network= document.getElementById("network1");
+	network.style.display = "inline";
+	var logo= document.getElementById("logo");
+	logo.style.display = "none";
+	var network= document.getElementById("network");
+	network.style.display = "none";
+	var logo= document.getElementById("logo2");
+	logo.style.display = "none";
+	var network= document.getElementById("network2");
+	network.style.display = "none";
+	var det = document.getElementById("det");
+	det = document.getElementById("det").innerHTML= "A woman who moves into an apartment next door to two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory."
+	var play = document.getElementById("div1");
+	var netflixp = document.getElementById("div2");
+	var hulup = document.getElementById("div3");
+	play.style.display = "inline";
+	hulup.style.display = "inline";
+	netflixp.style.display = "inline";
+	var link = document.getElementById("linkTrailer");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=WBb3fojgW0Q");
+	var hulu = document.getElementById("linkHulu");
+	hulu.setAttribute("href", "http://www.hulu.com/search?q=The+Big+Bang+Theory");
+	var homeText = document.getElementById("homeText");
+	var homeText1 = document.getElementById("homeText1");
+	homeText.style.display = "none";
+	homeText1.style.display = "none";
+
+
+}
+function out() {
+	var d = document.getElementById("pop");
+	d.style.height="90px";
+	var imgBack = document.getElementById('im1').src = "tbbtBack1.jpg"; 
+	
+}
+function popShow1() {
+	var a = document.getElementById("pop1");
+	a.style.height="200px";
+	var b = document.getElementById('im2').src = "ng.png";
+	var c = document.getElementById("body");
+	c.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)),url('ngBack.jpg')";
+	c.style.backgroundSize = "cover";
+	var logo1= document.getElementById("logo2");
+	logo1.style.display = "inline";
+	var network= document.getElementById("network2");
+	network.style.display = "inline";
+	var logo= document.getElementById("logo");
+	logo.style.display = "none";
+	var network= document.getElementById("network");
+	network.style.display = "none";
+	var logo= document.getElementById("logo1");
+	logo.style.display = "none";
+	var network= document.getElementById("network1");
+	network.style.display = "none";
+	var det = document.getElementById("det");
+	det = document.getElementById("det").innerHTML= "An ensemble comedy centering on a free-spirited young woman, her three male roommates and her best friend, as they navigate modern relationships and end up forming a charmingly dysfunctional or strangely functional family."
+	var play = document.getElementById("div1");
+	var netflixp = document.getElementById("div2");
+	var hulup = document.getElementById("div3");
+	play.style.display = "inline";
+	hulup.style.display = "inline";
+	netflixp.style.display = "inline";
+	// .style.display = "inline";	
+	var link = document.getElementById("linkTrailer");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=h8Hfph2wXIY");
+	var hulu = document.getElementById("linkHulu");
+	hulu.setAttribute("href", "http://www.hulu.com/new-girl");
+	var homeText = document.getElementById("homeText");
+	var homeText1 = document.getElementById("homeText1");
+	homeText.style.display = "none";
+	homeText1.style.display = "none";
+
+}
+
+function out1() {
+	var ng = document.getElementById("pop1");
+	ng.style.height="90px";
+	var imgBack = document.getElementById('im2').src = "ngBack1.jpg";
+}
+
+function popShow2() {
+	var a = document.getElementById("pop2");
+	a.style.height="200px";
+	var b = document.getElementById('im3').src = "bnnBack.jpg";
+	var c = document.getElementById("body");
+	c.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.1)),url('bnn2.jpg')";
+	c.style.backgroundSize = "cover";
+	var logo= document.getElementById("logo");
+	logo.style.display = "inline";
+	var network= document.getElementById("network");
+	network.style.display = "inline";
+	var logo= document.getElementById("logo1");
+	logo.style.display = "none";
+	var network= document.getElementById("network1");
+	network.style.display = "none";
+	var logo= document.getElementById("logo2");
+	logo.style.display = "none";
+	var network= document.getElementById("network2");
+	network.style.display = "none";
+	var det = document.getElementById("det");
+	det = document.getElementById("det").innerHTML= "Jake Peralta, a talented detective, along with his diverse but carefree team, struggles when a new commanding officer, Ray Holt, is appointed to their precinct.";
+	var play = document.getElementById("div1");
+	var netflixp = document.getElementById("div2");
+	var hulup = document.getElementById("div3");
+	play.style.display = "inline";
+	hulup.style.display = "inline";
+	netflixp.style.display = "inline";
+	var link = document.getElementById("linkTrailer");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=GdlSE9ISRBE");
+	var hulu = document.getElementById("linkHulu");
+	hulu.setAttribute("href", "http://www.hulu.com/brooklyn-nine-nine");
+	var homeText = document.getElementById("homeText");
+	var homeText1 = document.getElementById("homeText1");
+	homeText.style.display = "none";
+	homeText1.style.display = "none";
+}
+
+function out2() {
+	var d = document.getElementById("pop2");
+	d.style.height="90px";
+	var imgBack = document.getElementById('im3').src = "suitsBack1.jpg"; 
+}
